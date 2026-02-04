39ee9479811f291706a32c5874671daa76e7d1f3
edumateaibusiness-code <edumateaibusiness@gmail.com>
2026-02-04 20:03:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 import os
 
 class Config:
-    # Render environment variables se seedha data lega
-    API_ID = int(os.environ.get("API_ID", "0")) 
+    API_ID = int(os.environ.get("API_ID", "0"))
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     MONGO_URI = os.environ.get("MONGO_URI", "")
