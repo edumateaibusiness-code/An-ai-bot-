f01eab52d6aabb679ccfa3bdb5f4877272eebbac
edumateaibusiness-code <edumateaibusiness@gmail.com>
2026-02-04 19:56:05 +0530

    Refactor config.py to remove dotenv and update API_ID
    Removed dotenv loading and updated API_ID default value.

--- config.py
+++ config.py
@@ -1,10 +1,8 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", ""))
+    # Render environment variables se seedha data lega
+    API_ID = int(os.environ.get("API_ID", "0")) 
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     MONGO_URI = os.environ.get("MONGO_URI", "")
